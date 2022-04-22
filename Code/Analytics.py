52b9b8752d8206574444c41d91d5d26340ba2050
Thunfischpirat <christiansinger09@gmail.com>
2022-04-22 11:35:12 +0200

    Calculate expected deaths and minor works
    1. Add script for calculating the expected number of deaths for 2020-2022 via a model trained on fourier features and a linear trend.
    
    2. Put the extrapolation of the share of  deaths of the three 5-year age brackets on the total deaths of between 15 and 30 years in a separate script called "Extrapolate Deaths".
    
    3. Minor modifications of the function eurostat_loader() taking into account leap years. #30

--- Code/Analytics.py
+++ Code/Analytics.py
@@ -1,4 +1,6 @@
 import os
+from scipy import signal
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
@@ -130,51 +132,32 @@ def total_deaths(start: int, end: int, names: list, standardize=True, data=data,
         return pd.DataFrame(data=data)
 
 
+
 def eurostats_loader():
     relative_path = os.getcwd()[:-4] + "\\Data"
     data = pd.read_csv(f"{relative_path}\\Germany Weekly Total Deaths.csv",index_col=0)
     data = data[data != ":"].dropna()
-    data["Value"] = data["Value"].str.replace(",","").astype("int")
+    data["Value"] = data["Value"].str.replace("," , "")
+    data["Value"] = data["Value"].apply(lambda value: value + "0"*(5-len(value))).astype("int")
+    
     data.index = data.index.map(lambda date: date.replace("W","/")+"/6")
     data.index = pd.to_datetime(data.index,format="%Y/%W/%w")
+    data.index.name = "Date"
+    
+    data = data[~data.index.duplicated()]
     return data
 
 
 
 if __name__ == "__main__":
-    Weekly_Deaths = eurostats_loader()
-    # # Get yearly total deaths between 1998 and 2020.
-    # yearly_deaths = data.loc[[[year,"Insgesamt"] for year in range(1998,2021)]]
-    
-    # # Share of under one year olds of total deaths of males and females between age 0 and 15. 
-    # male_juvenile_deaths = yearly_deaths["männlich"][["unter 1 Jahr","1 bis unter 15 Jahre"]]
-    # female_juvenile_deaths = yearly_deaths["weiblich"][["unter 1 Jahr","1 bis unter 15 Jahre"]]
-    
-    # male_juvenile_deaths["ratio"] = male_juvenile_deaths["unter 1 Jahr"] / (male_juvenile_deaths["unter 1 Jahr"] +  male_juvenile_deaths["1 bis unter 15 Jahre"])
-    # female_juvenile_deaths["ratio"] = female_juvenile_deaths["unter 1 Jahr"] / (female_juvenile_deaths["unter 1 Jahr"] +  female_juvenile_deaths["1 bis unter 15 Jahre"])
-    
-    # # Same procedures for people between 15 and 30.
-    # male_young_deaths = yearly_deaths["männlich"][["15 bis unter 20 Jahre", "20 bis unter 25 Jahre", "25 bis unter 30 Jahre"]]
-    # female_young_deaths = yearly_deaths["weiblich"][["15 bis unter 20 Jahre", "20 bis unter 25 Jahre", "25 bis unter 30 Jahre"]]
-
-    # male_young_deaths["15 bis unter 20 Jahre"] = male_young_deaths["15 bis unter 20 Jahre"]/male_young_deaths.sum(axis=1)
-    # female_young_deaths["15 bis unter 20 Jahre"] = female_young_deaths["15 bis unter 20 Jahre"]/female_young_deaths.sum(axis=1)
-
-    # male_young_deaths["20 bis unter 25 Jahre"] = male_young_deaths["20 bis unter 25 Jahre"]/male_young_deaths.sum(axis=1)
-    # female_young_deaths["20 bis unter 25 Jahre"] = female_young_deaths["20 bis unter 25 Jahre"]/female_young_deaths.sum(axis=1)
-
-    # male_young_deaths["25 bis unter 30 Jahre"] = 1 - (male_young_deaths["20 bis unter 25 Jahre"] + male_young_deaths["15 bis unter 20 Jahre"])
-    # female_young_deaths["25 bis unter 30 Jahre"] = 1 - (male_young_deaths["20 bis unter 25 Jahre"] + male_young_deaths["15 bis unter 20 Jahre"])
-    
-    # # Add index for plotting.
-    # male_young_deaths.index = female_young_deaths.index = [year for year in range(1998,2021)]
-    # # Plot development of shares.
-    # male_young_deaths.plot(marker="o",markersize=4, title="Anteil an den männlichen Toten zwischen 15-30 Jahren", xlabel="Jahr", ylabel="Anteil")
-    # female_young_deaths.plot(marker="o",markersize=4, title="Anteil an den weiblichen Toten zwischen 15-30 Jahren", xlabel="Jahr", ylabel="Anteil")
-    # # Take mean of the last three years in order to calculate deaths for 2021.
-    # n_deaths_2021_males = 1790
-    # n_deaths_2021_females = 1127
-    # print(f"Mean of last three years for males: {male_young_deaths.loc[[2018,2019,2020]].mean()} * {n_deaths_2021_males}\n")
-    # print(f"Mean of last three years for females: {female_young_deaths.loc[[2018,2019,2020]].mean()} * {n_deaths_2021_females}")
 
     
+    # FF = abs((np.fft.rfft(Deaths_detrended)/np.sqrt(len(Deaths_detrended))))**2
+    # P = 4/len(Deaths_detrended) * FF
+    # f = np.arange(int(len(Deaths_detrended)/2)+1) / int(len(Deaths_detrended))    
+                                  
+    # fig1 = plt.figure()
+    # plt.xlabel("X")
+    # plt.ylabel("f(X)")
+    # plt.plot(f,P)
+    pass
---
+++ Code/Expected Deaths.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 08:42:27 2022
+
+@author: Admin
+"""
+
+from Analytics import eurostats_loader
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+from scipy.signal import periodogram
+from sklearn.linear_model  import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["#0645ad", "#800020"]) 
+
+#%% Create Data
+Weekly_Deaths = eurostats_loader()
+Weekly_Deaths = Weekly_Deaths[Weekly_Deaths.index.year >= 2008]
+
+Weekly_Deaths_train = Weekly_Deaths[Weekly_Deaths.index.year < 2019]
+Weekly_Deaths_val = Weekly_Deaths[Weekly_Deaths.index.year == 2019]
+Weekly_Deaths_test = Weekly_Deaths[Weekly_Deaths.index.year > 2019]
+
+
+#%% Periodogram of Series
+fs = pd.Timedelta("1Y") / pd.Timedelta("1W")
+f, Pxx = periodogram(Weekly_Deaths_train.Value, fs=fs)
+plt.plot(f, Pxx)
+
+#%% Modeling of Time Series
+
+fourier = CalendarFourier(freq="A", order=2)
+
+
+y_train = Weekly_Deaths_train.Value
+
+dp = DeterministicProcess(index=y_train.index.to_period("W"),
+                          constant=True,               # dummy feature for bias (y-intercept)
+                          order=1,                     # trend (order 1 means linear)
+                          additional_terms=[fourier],  # annual seasonality (fourier)
+                          drop=True,)                  # drop terms to avoid collinearity
+
+X_train = dp.in_sample()
+
+val_index = Weekly_Deaths_val.index
+X_val = dp.out_of_sample(len(val_index), val_index)
+
+#%% Fit Models and Plot Results.
+
+Models = [LinearRegression(fit_intercept=False),
+          GradientBoostingRegressor(max_depth=1)]
+
+num_models = len(Models)
+fig, axes = plt.subplots(num_models,2,layout="constrained",sharex="col",figsize = (15,8))
+
+for i,model in enumerate(Models):
+    
+    model.fit(X_train,y_train)
+    y_pred = pd.Series(model.predict(X_train).reshape(-1), index=y_train.index)
+    y_val = pd.Series(model.predict(X_val).reshape(-1), index=val_index)
+    
+    mse = np.mean(np.sqrt((y_val-Weekly_Deaths_val.Value)**2))
+    
+    ax_train = axes[i,0]
+    #ax_train.set_title(type(model).__name__)
+    y_pred.plot(ax=ax_train)
+    Weekly_Deaths_train.Value.plot(ax=ax_train)
+    
+    ax_val = axes[i,1]
+    ax_val.axes.annotate(f"MSE: {mse:.2f}", xy=(0.8,0.9), xycoords='axes fraction', fontsize=10)
+    y_val.plot(ax=ax_val)
+    Weekly_Deaths_val.Value.plot(ax=ax_val)
+    
+#%% Calculate Excess Deaths with best model.
+
+y_comb_train = Weekly_Deaths[Weekly_Deaths.index.year < 2020]
+
+dp = DeterministicProcess(index=y_comb_train.index.to_period("W"),
+                          constant=True,               # dummy feature for bias (y-intercept)
+                          order=1,                     # trend (order 1 means linear)
+                          additional_terms=[fourier],  # annual seasonality (fourier)
+                          drop=True,)                  # drop terms to avoid collinearity
+
+
+X_comb_train = dp.in_sample()
+
+model = GradientBoostingRegressor(max_depth=1)
+model.fit(X_comb_train, y_comb_train)
+y_comb_pred = pd.Series(model.predict(X_comb_train).reshape(-1), index=y_comb_train.index)
+
+
+test_index = Weekly_Deaths_test.index
+X_test = dp.out_of_sample(len(test_index), test_index)
+y_test = pd.Series(model.predict(X_test).reshape(-1), index=test_index)
+excess_deaths = np.sum(Weekly_Deaths_test.Value-y_test)
+
+fig_test, ax_test = plt.subplots()
+ax_test.axes.annotate(f"Estimated Excess Deaths: {excess_deaths:.0f}", xy=(0.55,0.9), xycoords='axes fraction', fontsize=8)
+y_test.plot(ax=ax_test)
+Weekly_Deaths_test.Value.plot(ax=ax_test)
+
+
+
+
+
